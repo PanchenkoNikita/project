729000e013b29366aa19973de35157b23a542ddb
PanchenkoNikita <crazy2003feog@gmail.com>
2024-05-30 21:23:58 +0500

    Add files via upload

---
+++ swiper.config.js
@@ -0,0 +1,50 @@
+// core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+
+//   Navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+
+    // when window width is >= 450px / xs
+    450: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 640px / sm
+    640: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px / md
+    768: {
+      slidesPerView: 3,
+    }
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,120 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./project/**/*.{html,js,php}"],
+  theme: {
+    screens: {
+      'xs': '450px',
+      // => @media (min-width: 320px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      gridTemplateColumns: {
+        'fill': 'repeat(auto-fill, minmax(0, 10fr))',
+        'fit': 'repeat(auto-fit, minmax(0, 1fr))',
+      },
+      aspectRatio: {
+        '9/12': '9 / 12',
+      },
+      fontSize: {
+        'xxs': '10px',
+      },
+      fontFamily: {
+        raleway: ['Raleway'],
+        martel: ['Martel'],
+      },
+      strokeWidth: {
+        '1': '1px',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '5': '5px',
+        '6': '6px',
+        '7': '7px',
+        '8': '8px',
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+      {
+        base: {
+          "primary": "#ffffff",
+          "accent-content": "#d8ab0c",
+          "secondary": "#d32727",
+          "accent": "#ffc700",
+          "neutral": "#f6f6f6",
+          "base-100": "#ffffff",
+          "info": "#38bdf8",
+          "success": "#00e8aa",
+          "warning": "#fcd34d",
+          "error": "#ff6382",
+
+          "--rounded-btn": "0.8rem",
+        },
+
+        red: {
+          "primary": "#ffffff",
+          "accent-content": "#ac2020",
+          "secondary": "#d32727",
+          "accent": "#d32727",
+          "neutral": "#f6f6f6",
+          "base-100": "#ffffff",
+          "info": "#38bdf8",
+          "success": "#00e8aa",
+          "warning": "#fcd34d",
+          "error": "#ff6382",
+
+          "--rounded-btn": "0rem",
+          "--rounded-box": "0rem",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
